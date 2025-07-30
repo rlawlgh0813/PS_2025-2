3bd14160734fadf4816b2b5951ccad17ab7c5f59
jihodo <jihooo7@naver.com>
2025-07-30 23:39:15 +0900

    11279

---
+++ class/class3/11279.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+priority_queue<int> pq;
+
+void solve(){
+    cin >> n;
+    while(n--){
+        int a; cin >> a;
+        if(a == 0){
+            if(pq.empty()) cout << 0 << '\n';
+            else{
+                cout << pq.top() << '\n';
+                pq.pop();
+            }
+        }else{
+            pq.push(a);
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    solve();
+    return 0;
+}
