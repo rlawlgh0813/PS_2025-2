8499e3334446e1b224008408830f11b0f57ced8a
jihodo <jihooo7@naver.com>
2025-08-05 22:06:38 +0900

    2357

---
+++ class/class6/2357.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1e9
+typedef pair<int,int> pii;
+int n,m;
+int a[100004];
+vector<pii> v(4 * 100004);
+
+pii init(int s, int e, int node){
+    if(s == e){
+        v[node].first = v[node].second = a[s];
+        return v[node];
+    }
+    int m = (s + e) / 2;
+    pii left = init(s, m, 2*node);
+    pii right = init(m+1, e, 2*node+1);
+    v[node].first = min(left.first, right.first);
+    v[node].second = max(left.second, right.second);
+    return v[node];
+}
+
+pii val(int s, int e, int l, int r, int node){
+    if(r < s || l > e) return make_pair(INF,-INF);
+    if(l <= s && e <= r) return v[node];
+    int m = (s + e) / 2;
+    pii left = val(s,m,l,r,node*2);
+    pii right = val(m+1,e,l,r,node*2+1);
+    return make_pair(min(left.first, right.first), max(left.second, right.second));
+}
+
+void solve(){
+    cin >> n >> m;
+    for(int i=0; i<n; i++) cin >> a[i];
+    init(0,n-1,1);
+
+    while(m--){
+        int p,q; cin >> p >> q;
+        pii ret = val(0,n-1,p-1,q-1,1);
+        cout << ret.first << " " << ret.second << '\n';
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    solve();
+    return 0;
+}
---
+++ class/class6/2533.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+int ret[2], vis[1000004];
+vector<int> adj[1000004];
+
+void go(int here, int parent, int depth){
+    vis[here] = 1;
+    ret[depth]++;
+    for(auto there : adj[here]){
+        if(there == parent || vis[there]) continue;
+        go(there, here, depth^1);
+    }
+}
+
+void solve(){
+    cin >> n;
+    for(int i=0; i<n-1; i++){
+        int p,q; cin >> p >> q;
+        adj[p].push_back(q);
+        adj[q].push_back(p);
+    }
+    go(1,0,0);
+
+    cout << ret[0] << " " << ret[1] << '\n';
+    cout << min(ret[0],ret[1]);
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    solve();
+    return 0;
+}
