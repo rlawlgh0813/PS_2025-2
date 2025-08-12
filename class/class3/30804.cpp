4ccf3979ecb672f97a6913bc5764f6479e0022e9
jihodo <jihooo7@naver.com>
2025-08-12 21:46:56 +0900

    30804

--- class/class3/30804.cpp
+++ class/class3/30804.cpp
@@ -1,26 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
-int a[200004];
+int n,l,r,ret;
+int a[200004],num[10];
 
-int go(int target){
-    int l = 0, r = n-1;
-    while(l < r){
-        
-    }
+int check(){
+    int cnt=0;
+    for(int i=1; i<10; i++) if(num[i]) cnt++;
+    return cnt;
 }
 
 void solve(){
     cin >> n;
     for(int i=0; i<n; i++) cin >> a[i];
 
-    int ret = 1e9;
-    for(int i=0; i<8; i++) for(int j=0; j<9; j++) ret = min(ret, go((1<<i)&(1<<j)));
+    while(r < n){
+        num[a[r]]++;
+        while(check() > 2){
+            num[a[l]]--;
+            l++;
+        }
+        ret = max(ret, r-l+1);
+        r++;
+    }
     cout << ret;
 }
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     solve();
-    return;
+    return 0;
 }
